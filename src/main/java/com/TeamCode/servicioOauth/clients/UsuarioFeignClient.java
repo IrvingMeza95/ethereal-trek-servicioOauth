6436a5cab9270b0646a680548735801c9d2c8292
Irving Meza <irvingmeza95@gmail.com>
2024-03-06 13:45:10 -0600

    commit de actualizacion.

--- src/main/java/com/TeamCode/servicioOauth/clients/UsuarioFeignClient.java
+++ src/main/java/com/TeamCode/servicioOauth/clients/UsuarioFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="servicioUsuarios")
+@FeignClient(name="gestionEmpCli")
 public interface UsuarioFeignClient {
 	@GetMapping("/auth/{param}")
 	Usuario buscar(@RequestParam String param);
