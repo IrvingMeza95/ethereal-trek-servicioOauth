ebfb070a881aa4a2bc4786ae7a60bed8600e1388
Irving Meza <irvingmeza95@gmail.com>
2024-03-14 13:54:00 -0500

    Se guitaron los setters y getters de los modelos.

--- src/main/java/com/TeamCode/servicioOauth/models/Role.java
+++ src/main/java/com/TeamCode/servicioOauth/models/Role.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Role {
     private String id;
     private Roles nombre;
--- src/main/java/com/TeamCode/servicioOauth/models/Usuario.java
+++ src/main/java/com/TeamCode/servicioOauth/models/Usuario.java
@@ -7,7 +7,6 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Usuario {
     private String id;
     private String nombre;
@@ -19,84 +18,4 @@ public class Usuario {
     private Boolean enabled;
     private Integer intentos;
     private List<Role> roles;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public Integer getIntentos() {
-        return intentos;
-    }
-
-    public void setIntentos(Integer intentos) {
-        this.intentos = intentos;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
 }
